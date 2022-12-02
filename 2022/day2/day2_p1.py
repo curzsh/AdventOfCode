bc8dc20205f73196f20d33fc96255cfaf9a04b76
curzsh <nstaub91@gmail.com>
2022-12-01 19:09:15 -0600

    moved to yearly organizationa nd solved day1 of 2022

---
+++ 2022/day2/day2_p1.py
@@ -0,0 +1,14 @@
+
+with open('2022/day2/day2_input', 'r') as file:
+  lines = [line.rstrip() for line in file]
+
+index = 0
+elf_calories = [0]
+for line in lines:
+  if not line:
+    index += 1
+    elf_calories.append(0)
+  else:
+    elf_calories[index] += int(line)
+
+print (max(elf_calories))
