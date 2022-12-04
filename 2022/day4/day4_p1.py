2779fe140c432915f7e8ec919670c8484ba84c0b
curzsh <nstaub91@gmail.com>
2022-12-04 10:59:55 -0600

    day4 done

---
+++ 2022/day4/day4_p1.py
@@ -0,0 +1,15 @@
+import intspan
+
+with open('2022\day4\day4_input', 'r') as file:
+  lines = [line.rstrip() for line in file]
+
+count = 0
+for line in lines:
+  work_shifts = line.split(',')
+  set1 = set(intspan.intspan(work_shifts[0]))
+  set2 = set(intspan.intspan(work_shifts[1]))
+
+  if set1.issubset(set2) or set2.issubset(set1):
+    count += 1
+  
+print (count)
