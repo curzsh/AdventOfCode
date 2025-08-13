ee40b565003fde9bdb78367699571084e8729449
curzsh <nstaub91@gmail.com>
2025-08-12 19:10:01 -0500

    optimize part2

--- 2025/day2/p2.py
+++ 2025/day2/p2.py
@@ -6,23 +6,33 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 with open('input', 'r') as file:
   lines = [line.rstrip() for line in file]
 
-
 def is_valid(seq):
-    increasing = all(seq[j] < seq[j+1] for j in range(len(seq)-1))
-    decreasing = all(seq[j] > seq[j+1] for j in range(len(seq)-1))
-    adjacent = all(1 <= abs(seq[j] - seq[j+1]) <= 3 for j in range(len(seq)-1))
+    if len(seq) < 2:
+        return False
+    increasing = True
+    decreasing = True
+    adjacent = True
+    for j in range(1, len(seq)):
+        if seq[j] > seq[j-1]:
+            decreasing = False
+        elif seq[j] < seq[j-1]:
+            increasing = False
+        if not (1 <= abs(seq[j] - seq[j-1]) <= 3):
+            adjacent = False
+        if not (increasing or decreasing) or not adjacent:
+            return False
     return (increasing or decreasing) and adjacent
 
 total = 0
 for line in lines:
     numbers = [int(num) for num in line.split()]
-    removed = False
+    valid_with_removal = False
     for idx in range(len(numbers)):
         temp_numbers = numbers[:idx] + numbers[idx+1:]
         if is_valid(temp_numbers):
-            removed = True
+            valid_with_removal = True
             break
-    if removed:
+    if valid_with_removal:
         total += 1
 
 print(total)
